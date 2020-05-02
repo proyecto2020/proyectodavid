7b868d0f63776f116b111d4f11d858015fc4aaad
jadrez <40375030+jadrez@users.noreply.github.com>
2020-05-02 14:00:40 -0500

    Datos Jason

---
+++ Entidades/Grados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Grados
+    {
+        int _idGrado;
+        string _Grado;
+    }
+}
