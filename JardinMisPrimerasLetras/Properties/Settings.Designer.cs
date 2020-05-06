e394791e96f5935a81a39a6d2a2aa4b3af08695d
davidMarin7 <64705122+davidMarin7@users.noreply.github.com>
2020-05-05 21:16:05 -0500

    Revert "ok"
    This reverts commit f20cd643f56bb4106078d9b3c462259596db6923.

--- JardinMisPrimerasLetras/Properties/Settings.Designer.cs
+++ JardinMisPrimerasLetras/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace JardinMisPrimerasLetras.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -49,28 +49,6 @@ namespace JardinMisPrimerasLetras.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=JAZON;Initial Catalog=Letras;Integrated Security=True")]
-        public string conexionDB2 {
-            get {
-                return ((string)(this["conexionDB2"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-5L9RO2S\\SQLEXPRESS;Initial Catalog=Letras;Integrated Security" +
-            "=True")]
-        public string conexionDB1 {
-            get {
-                return ((string)(this["conexionDB1"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-5L9RO2S\\SQLEXPRESS;Initial Catalog=Letras;Integrated Security" +
-            "=True")]
         public string conexionDB {
             get {
                 return ((string)(this["conexionDB"]));
