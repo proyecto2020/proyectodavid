deeb1fefa3c5f94fc8fd09b35d8cceb599600214
davidMarin7 <64705122+davidMarin7@users.noreply.github.com>
2020-05-05 21:24:08 -0500

    cambioColor

--- JardinMisPrimerasLetras/LetrasDataSetGestion1.Designer.cs
+++ JardinMisPrimerasLetras/LetrasDataSetGestion1.Designer.cs
@@ -875,7 +875,7 @@ SELECT idGestion, Grados, GrupoEscolar, Area, Materia FROM Gestion WHERE (idGest
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         private void InitConnection() {
             this._connection = new global::System.Data.SqlClient.SqlConnection();
-            this._connection.ConnectionString = global::JardinMisPrimerasLetras.Properties.Settings.Default.conexionDB;
+            this._connection.ConnectionString = global::JardinMisPrimerasLetras.Properties.Settings.Default.conexionDB1;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
